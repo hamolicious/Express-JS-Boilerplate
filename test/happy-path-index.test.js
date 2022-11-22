3d0cf89924658fe2e22780a8a301fbcea32876a3
hamolicious <kickfliptv8@gmail.com>
2022-11-22 09:32:50 +0000

    made test case make sense

--- test/happy-path-index.test.js
+++ test/happy-path-index.test.js
@@ -3,7 +3,7 @@ import { get } from './helpers/http.js'
 
 describe('Index', function () {
   describe('GET /', function () {
-    it('should Hello World', async function () {
+    it('should have status 200', async function () {
       const data = await get('/');
 			equal(data.status, 200);
     });
